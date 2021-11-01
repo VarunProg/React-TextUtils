2786c7bec86907a21a5e79917a92bab2eaa01b73
VarunProg <89865506+VarunProg@users.noreply.github.com>
2021-11-01 14:43:10 +0100

    set dark and light mode

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import './App.css';
 import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import React, { useState } from 'react';
+
 
 function App() {
-  return (
+  const [mode, setmode] = useState('light'); // to check the dark mode
+  const [text, settext] = useState('Enable Dark Mode')
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setmode('dark');
+      settext('Enable Light Mode');
+    }
+    else{
+      setmode('light');
+      settext('Enable Dark Mode');
+    }
+  }
+   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar title="TextUtils" aboutText="About TextUtils"  mode={mode} toggleMode={toggleMode} text={text}/>
       <div className="container my-3">
         <TextForm heading="enter the text to analyze below" />
         {/* <About/> */}
