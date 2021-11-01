bf0c04407c61c31cb65d495836f4337f630d1f2a
VarunProg <89865506+VarunProg@users.noreply.github.com>
2021-11-01 17:31:07 +0100

    set alert

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -21,7 +21,7 @@ export default function Navbar(props) {
           </ul>
           <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' :'light'}`}>
             <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode}/>
-            <label className="form-check-label" for="flexSwitchCheckDefault">{props.text}</label>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.text}</label>
           </div>
         </div>
       </div>
